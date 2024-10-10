31fa98ca7ff12b709c99c6f1f374b044bec78861
azazaza <sdt2395gp0001@edu.sca.ac.jp>
2024-10-10 12:34:02 +0900

    敵のスクリプト追加

--- Assets/rui/Scripts/RuiPlayerManager.cs
+++ Assets/rui/Scripts/RuiPlayerManager.cs
@@ -16,22 +16,22 @@ public class RuiPlayerManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyUp(KeyCode.W))
+        if (Input.GetKeyDown(KeyCode.W))
         {
             transform.position += new Vector3(0, 1, 0);
         }
 
-        if (Input.GetKeyUp(KeyCode.S))
+        if (Input.GetKeyDown(KeyCode.S))
         {
             transform.position += new Vector3(0, -1, 0);
         }
 
-        if (Input.GetKeyUp(KeyCode.D))
+        if (Input.GetKeyDown(KeyCode.D))
         {
             transform.position += new Vector3(1, 0, 0);
         }
 
-        if (Input.GetKeyUp(KeyCode.A))
+        if (Input.GetKeyDown(KeyCode.A))
         {
             transform.position += new Vector3(-1, 0, 0);
         }
