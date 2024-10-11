b0457b706bf9999484182bdb93979d698c14dca8
syuuto <SDT2395GP0019@edu.sca.ac.jp>
2024-10-11 14:10:12 +0900

    ノーツに合わせてスペースで音が鳴る

--- Assets/syu-to/Scripts/NoteSpawn.cs
+++ Assets/syu-to/Scripts/NoteSpawn.cs
@@ -1,29 +1,39 @@
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class NoteSpawn : MonoBehaviour
 {
-    [SerializeField] GameObject objectToSpawn; // 出現させるオブジェクト
-    [SerializeField] float spawnInterval = 2f; // 出現間隔
-    [SerializeField] float moveSpeed = 2f; // 移動速度
-    [SerializeField] float spawnPosition = 5f; // スポーン位置
+    [SerializeField] GameObject objectToSpawn; //出現させるオブジェクト
+    [SerializeField] float spawnInterval = 2f; //出現間隔
+    [SerializeField] float moveSpeed = 2f; //移動速度
+    [SerializeField] float spawnPosition = 5f; //スポーン位置
+    [SerializeField] AudioClip sound1;
+    private AudioSource audioSource; //AudioSourceコンポーネントを格納
 
     private void Start()
     {
-        // 定期的にSpawnObjectsメソッドを呼び出す
-        InvokeRepeating("SpawnObjects", 0f, spawnInterval);
+        audioSource = GetComponent<AudioSource>(); //AudioSourceコンポーネントを取得
+
+        InvokeRepeating("SpawnObjects", 0f, spawnInterval); //一定間隔でSpawnObjectsメソッドを呼び出す
     }
 
     private void SpawnObjects()
     {
-        // 左側からオブジェクトをスポーン
-        Vector3 spawnPositionLeft = new Vector3(-spawnPosition, transform.position.y, transform.position.z);
-        GameObject spawnedObjectLeft = Instantiate(objectToSpawn, spawnPositionLeft, Quaternion.identity);
-        spawnedObjectLeft.AddComponent<Note>().Initialize(moveSpeed, Vector2.right);
-
-        // 右側からオブジェクトをスポーン
-        Vector3 spawnPositionRight = new Vector3(spawnPosition, transform.position.y, transform.position.z);
+        Vector3 spawnPositionRight = new Vector3(spawnPosition, transform.position.y, transform.position.z); //右側からオブジェクトをスポーン
         GameObject spawnedObjectRight = Instantiate(objectToSpawn, spawnPositionRight, Quaternion.identity);
         spawnedObjectRight.AddComponent<Note>().Initialize(moveSpeed, Vector2.left);
+
+        Vector3 spawnPositionLeft = new Vector3(-spawnPosition, transform.position.y, transform.position.z); //左側からオブジェクトをスポーン
+        GameObject spawnedObjectLeft = Instantiate(objectToSpawn, spawnPositionLeft, Quaternion.identity);
+        spawnedObjectLeft.AddComponent<Note>().Initialize(moveSpeed, Vector2.right);
+    }
+
+    public void PlaySound()
+    {
+        if (audioSource != null && sound1 != null) //AudioSourceと音声クリップが設定されているとき
+        {
+            audioSource.PlayOneShot(sound1); //音を再生
+        }
     }
 }
