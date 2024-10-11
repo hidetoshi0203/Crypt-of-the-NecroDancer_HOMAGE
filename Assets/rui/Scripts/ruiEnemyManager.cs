c4ec37ac07eaf57ad29b7c4ed584fa6d8bd9a2a9
OharaYuta <sdt22cge0003@edu.sca.c.jp>
2024-10-11 16:43:12 +0900

    敵の移動のスクリプト少し書いた

--- Assets/rui/Scripts/ruiEnemyManager.cs
+++ Assets/rui/Scripts/ruiEnemyManager.cs
@@ -1,20 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.UI;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class ruiEnemyManager : MonoBehaviour
 {
-    ruiEnemyEnumes enemyEnumes;
+    [SerializeField]
+    ruiEnemyEnums enemyEnums;
+    public GameObject slime;
 
     // Start is called before the first frame update
     void Start()
     {
-        enemyEnumes = GetComponent<ruiEnemyEnumes>();
+        enemyEnums = GetComponent<ruiEnemyEnums>();
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        if (slime.name == "Slime")
+        {
+            enemyEnums.EnemyMoveUp();
+        }
     }
 }
