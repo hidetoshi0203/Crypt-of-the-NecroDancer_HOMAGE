0816cfa39da43ead3b922ef95d6d2d06b0a614eb
kouriki <sdt2395gp0010@edu.sca.ac.jp>
2025-01-31 14:54:00 +0900

    スクリプトを少し統合した

--- Assets/toshi/Scripts/CheckAliveScripts.cs
+++ Assets/toshi/Scripts/CheckAliveScripts.cs
@@ -8,10 +8,5 @@ public class CheckAliveScripts : MonoBehaviour
     //上記のスクリプトがアタッチされているオブジェクトの生死のフラグ
     public bool isAliveToshiPlayerScr = true; // RuitoshiPlayerのスクリプト0
     public bool isAliveEnemyManagerScr = true; // EnemyManagerのスクリプト0
-    public bool isAliveRLSlimeScr = true; // RuiRL_Slimeのスクリプト
-    public bool isAliveTDSlimeScr = true; // RuiTD_Slimeのスクリプト
-    public bool isAliveruiRLZombieScr = true; // RuiRL_Zombieのスクリプト
-    public bool isAliveruiTDZombieScr = true; // RuiTD_Zombieのスクリプト
-    public bool isAliveCentaurScr = true; // RuiCentaurのスクリプト
     public bool isAliveItemScr = true; // Itemのスクリプト
 }
