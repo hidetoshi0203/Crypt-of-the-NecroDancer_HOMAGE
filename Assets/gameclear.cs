dc6ae757bec14d4119a124256f83278bacd0f42f
aaaaaaaaaaaaaaaaaaaaaaz <SDT2395GP0001@edu.sca.ac.jp>
2025-02-02 13:27:35 +0900

    a

--- Assets/gameclear.cs
+++ Assets/gameclear.cs
@@ -3,25 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class GameOverManager : MonoBehaviour
+public class gameclear : MonoBehaviour
 {
-    AudioSource audioSource;
-    //[SerializeField] private AudioClip GameOverBGM;
-
     // Start is called before the first frame update
     void Start()
     {
-        //audioSource = GetComponent<AudioSource>();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        //audioSource.PlayOneShot(GameOverBGM);
-
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            SceneManager.LoadScene("testScene");
+            SceneManager.LoadScene("title");
         }
     }
 }
